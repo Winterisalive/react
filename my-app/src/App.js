8798bd867ab7ab2da7c8f0a9f265739c643a3dd0
Winterisalive <adriankacperkowalski@gmail.com>
2023-05-02 14:33:25 +0200

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+<h1  className="btn-shine">
+  Adrian Kowalski 
+numer indeksu: 118897</h1>
+    </div>
+  );
+}
+
+export default App;
